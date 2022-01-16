224f09778ddc815e3689e3a7e9c8f7e815008552
sa46lll <9812smh@naver.com>
2022-01-16 23:25:40 +0900

    executorService singleThread 추가

--- src/main/java/toby/live/Ob.java
+++ src/main/java/toby/live/Ob.java
@@ -1,6 +1,8 @@
 package toby.live;
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SuppressWarnings("deprecation")
 public class Ob {
@@ -22,13 +24,17 @@ public class Ob {
 
 			@Override
 			public void update(Observable o, Object arg) {
-				System.out.println(arg);
+				System.out.println(Thread.currentThread().getName() + " " + arg);
 			}
 		};
 		
 		IntObservable io = new IntObservable();
 		io.addObserver(ob);
 		
-		io.run();
+		ExecutorService es = Executors.newSingleThreadExecutor();
+		es.execute(io);
+		
+		System.out.println(Thread.currentThread().getName() + "   Exit");
+		es.shutdown();
 	}
 }
