6986ec25fc7413b60fc5d298712a2ef5a2e2f24b
sa46lll <9812smh@naver.com>
2022-01-21 23:46:24 +0900

    feat: Interval Ex

---
+++ src/main/java/toby/live2/IntervalEx.java
@@ -0,0 +1,94 @@
+package toby.live2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class IntervalEx {
+    public static void main(String[] args) {
+        Publisher<Integer> pub = sub -> {
+            sub.onSubscribe(new Subscription() {
+                int no = 0;
+                volatile boolean canceled = false;
+
+                @Override
+                public void request(long n) {
+                    ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+                    exec.scheduleAtFixedRate(() -> { // 일정한 시간 간격을 두고 계속 수행하게 함.
+                        if (canceled){
+                            exec.shutdown();
+                            return;
+                        }
+                        sub.onNext(no++);
+                    }, 0, 300, TimeUnit.MILLISECONDS);
+                }
+
+                @Override
+                public void cancel() {
+                    canceled = true;
+                }
+            });
+        };
+
+        Publisher<Integer> takePub = sub -> {
+            pub.subscribe(new Subscriber<Integer>() {
+                int count = 0;
+                Subscription subsc;
+
+                @Override
+                public void onSubscribe(Subscription s) {
+                    subsc = s;
+                    sub.onSubscribe(s);
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    sub.onNext(integer);
+                    if (++count >= 5) {
+                        subsc.cancel();
+                    }
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    sub.onError(t);
+                }
+
+                @Override
+                public void onComplete() {
+                    sub.onComplete();
+                }
+            });
+        };
+
+        takePub.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext: {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError: {}", t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        });
+    }
+}
