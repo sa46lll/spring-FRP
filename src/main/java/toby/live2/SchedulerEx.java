f6ebb5428781697b7e79ab20d97f36a2f473b944
sa46lll <9812smh@naver.com>
2022-01-21 19:32:41 +0900

    feat: PublishOn Scheculer

--- src/main/java/toby/live2/SchedulerEx.java
+++ src/main/java/toby/live2/SchedulerEx.java
@@ -5,6 +5,7 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -15,6 +16,7 @@ public class SchedulerEx {
             sub.onSubscribe(new Subscription() {
                 @Override
                 public void request(long n) {
+                    log.debug("request()");
                     sub.onNext(1);
                     sub.onNext(2);
                     sub.onNext(3);
@@ -31,13 +33,40 @@ public class SchedulerEx {
         };
         //pub
 
-        Publisher<Integer> subOnPub = sub -> {
+        /*
+        Publisher<Integer> subOnPub = sub -> { // subscribeOn 기능
             ExecutorService es = Executors.newSingleThreadExecutor();
             es.execute(() -> pub.subscribe(sub));
         };
+         */
+
+        Publisher<Integer> pubOnPub = sub -> { // publishOn 기능
+            pub.subscribe(new Subscriber<Integer>() {
+                ExecutorService es = Executors.newSingleThreadExecutor();
+                @Override
+                public void onSubscribe(Subscription s) {
+                    sub.onSubscribe(s);
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    es.execute(() -> sub.onNext(integer));
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    es.execute(() -> sub.onError(t));
+                }
+
+                @Override
+                public void onComplete() {
+                    es.execute(() -> sub.onComplete());
+                }
+            });
+        };
 
         //sub
-        subOnPub.subscribe(new Subscriber<Integer>() {
+        pubOnPub.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
                 log.debug("onSubscribe");
