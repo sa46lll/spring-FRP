36ce2539aaeaf9f184b06d6a71b12f325d428603
sa46lll <9812smh@naver.com>
2022-01-21 19:48:13 +0900

    feat: SubscribeOn, PublishOn Scheculer 적용

--- src/main/java/toby/live2/SchedulerEx.java
+++ src/main/java/toby/live2/SchedulerEx.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -33,16 +34,25 @@ public class SchedulerEx {
         };
         //pub
 
-        /*
         Publisher<Integer> subOnPub = sub -> { // subscribeOn 기능
-            ExecutorService es = Executors.newSingleThreadExecutor();
+            ExecutorService es = Executors.newSingleThreadExecutor(new CustomizableThreadFactory(){ // 스레드 이름 변경
+                @Override
+                public String getThreadNamePrefix() {
+                    return "subOn-";
+                }
+            });
             es.execute(() -> pub.subscribe(sub));
         };
-         */
 
         Publisher<Integer> pubOnPub = sub -> { // publishOn 기능
-            pub.subscribe(new Subscriber<Integer>() {
-                ExecutorService es = Executors.newSingleThreadExecutor();
+            subOnPub.subscribe(new Subscriber<Integer>() {
+                ExecutorService es = Executors.newSingleThreadExecutor(new CustomizableThreadFactory(){
+                    @Override
+                    public String getThreadNamePrefix() {
+                        return "pubOn-";
+                    }
+                });
+
                 @Override
                 public void onSubscribe(Subscription s) {
                     sub.onSubscribe(s);
