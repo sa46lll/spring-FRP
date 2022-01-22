ef8314655715f1d2c3cbff89f6942138949e00b9
sa46lll <9812smh@naver.com>
2022-01-22 23:35:50 +0900

    feat: FutureTask

--- src/main/java/toby/live3/FutureEx.java
+++ src/main/java/toby/live3/FutureEx.java
@@ -2,28 +2,40 @@ package toby.live3;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        Future<String> f = es.submit(() -> {
+        FutureTask<String> f = new FutureTask<String>(() -> {
             Thread.sleep(2000);
             log.info("Async");
             return "Hello";
-        });
+        }) {
+            @Override
+            protected void done() { // 비동기 작업 완료후에 호출
+                try {
+                    System.out.println(get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
+        es.execute(f);
+        es.shutdown();
+
+        /*
         System.out.println(f.isDone()); // 작업이 완료되면 true
         Thread.sleep(2100);
         log.info("Exit");
         System.out.println(f.isDone());
         System.out.println(f.get()); // Blocking
-
+         */
 
     }
 }
