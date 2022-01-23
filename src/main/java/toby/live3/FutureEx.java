e677ef7f65faf3d964df6e1e191114c6348c9915
sa46lll <9812smh@naver.com>
2022-01-24 03:23:02 +0900

    feat: CallbackFutureTask (SuccessCallback, ExceptionCallback)

--- src/main/java/toby/live3/FutureEx.java
+++ src/main/java/toby/live3/FutureEx.java
@@ -2,29 +2,54 @@ package toby.live3;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
 import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
+
+    interface SuccessCallback{
+        void onSuccess(String result);
+    }
+
+    interface ExceptionCallback {
+        void onError(Throwable t);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+        SuccessCallback sc;
+        ExceptionCallback ec;
+
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc, ExceptionCallback ec) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc); // null 이 아니면 그 값을 그대로 리턴함.
+            this.ec = Objects.requireNonNull(ec);
+        }
+
+        @Override
+        protected void done() {
+            try {
+                sc.onSuccess(get());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                ec.onError(e.getCause());
+            }
+        }
+    }
+
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        FutureTask<String> f = new FutureTask<String>(() -> {
+        CallbackFutureTask f = new CallbackFutureTask(() -> {
             Thread.sleep(2000);
+//            if (1==1) throw new RuntimeException("Async ERROR!!!"); // Error Test
             log.info("Async");
             return "Hello";
-        }) {
-            @Override
-            protected void done() { // 비동기 작업 완료후에 호출
-                try {
-                    System.out.println(get());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+        },
+                s -> System.out.println("Result: " + s),
+                e -> System.out.println("Error: " + e.getMessage()));
 
         es.execute(f);
         es.shutdown();
