3cf3021a5a810d35b90e07600d1ef950ec486ec1
sa46lll <9812smh@naver.com>
2022-01-24 12:57:42 +0900

    feat: Async 적용 - Future

--- src/main/java/toby/sandbox/Toby008Application.java
+++ src/main/java/toby/sandbox/Toby008Application.java
@@ -8,19 +8,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Future;
 
 @SpringBootApplication
 @Slf4j
+@EnableAsync
 public class Toby008Application {
 
     @Component
     public static class MyService {
         @Async
-        public String hello() throws InterruptedException {
+        public Future<String> hello() throws InterruptedException {
             log.info("hello()");
-            Thread.sleep(1000);
-            return "Hello";
+            Thread.sleep(2000);
+            return new AsyncResult<>("Hello");
         }
     }
 
@@ -35,8 +40,9 @@ public class Toby008Application {
     ApplicationRunner run(){
         return args -> {
             log.info("run()");
-            String res = myService.hello();
-            log.info("exit: " + res);
+            Future<String> f = myService.hello();
+            log.info("exit: " + f.isDone());
+            log.info("result: " + f.get());
         };
     }
 
