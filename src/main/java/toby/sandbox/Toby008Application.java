4e38efed95bb2aeafa04b1ec3c709a8e1cd7ccf8
sa46lll <9812smh@naver.com>
2022-01-25 09:39:07 +0900

    feat: Async - ThreadPoolTaskExecutor

--- src/main/java/toby/sandbox/Toby008Application.java
+++ src/main/java/toby/sandbox/Toby008Application.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @Slf4j
@@ -22,13 +24,25 @@ public class Toby008Application {
 
     @Component
     public static class MyService {
-        @Async
+        @Async(value = "tp")
         public ListenableFuture<String> hello() throws InterruptedException {
             log.info("hello()");
             Thread.sleep(2000);
             return new AsyncResult<>("Hello");
         }
     }
+
+    @Bean
+    ThreadPoolTaskExecutor tp(){
+        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+        te.setCorePoolSize(10);
+        te.setMaxPoolSize(100);
+        te.setQueueCapacity(200);
+        te.setThreadNamePrefix("mythread");
+        te.initialize();
+
+        return te;
+    };
 
     public static void main(String[] args) {
         try (ConfigurableApplicationContext c = SpringApplication.run(Toby008Application.class, args)){
